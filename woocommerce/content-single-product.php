56109905313862e971e57270683e3d6230a0d0bd
Dinah Chen <dinah.chen.ccy@gmail.com>
2020-10-26 14:55:42 -0700

    going back and forth on styles

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -34,7 +34,7 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'container-fluid container-lg', $product ); ?>>
 
 
 
